ba4b11ac230508711c026324e314d65e468b0425
SIGAE\alstn <alstn1601@naver.com>
2018-05-07 13:42:55 +0900

    예외문 추가

--- app/views.py
+++ app/views.py
@@ -20,14 +20,27 @@ def message(request):
     json_str = (request.body).decode('utf-8')
     received_json = json.loads(json_str)
     content_name = received_json['content']
+    type_name = received_json['type']
 
-    return JsonResponse(
-        {
-            'message': {
-                'text': app.AuctionSearch.SearchAuction(content_name)
-            },
-            'keyboard': {
-                'type': 'text'
+    if type_name == 'text':
+        return JsonResponse(
+            {
+                'message': {
+                    'text': app.AuctionSearch.SearchAuction(content_name)
+                },
+                'keyboard': {
+                    'type': 'text'
+                }
             }
-        }
-    )
+        )
+    else:
+        return JsonResponse(
+            {
+                'message': {
+                    'text': '텍스트만 입력하주시길 바랍니다'
+                },
+                'keyboard': {
+                    'type': 'text'
+                }
+            }
+        )
