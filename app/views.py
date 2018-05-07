ed1c5f4b7196f7919ab36fa255d60b0d43520da6
SIGAE\alstn <alstn1601@naver.com>
2018-05-07 13:37:45 +0900

    경매장 검색기능 구현

--- app/views.py
+++ app/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from django.views.decorators.csrf import csrf_exempt
+import json
+import app.AuctionSearch
 # Create your views here.
+
+
 def keyboard(request):
     return JsonResponse(
         {
-            'message' : {
-                'text' : '검색하실 아이템의 이름을 말해주세요'
-            },
             'type': 'text'
+        }
+    )
+
+
+@csrf_exempt
+def message(request):
+
+    json_str = (request.body).decode('utf-8')
+    received_json = json.loads(json_str)
+    content_name = received_json['content']
+
+    return JsonResponse(
+        {
+            'message': {
+                'text': app.AuctionSearch.SearchAuction(content_name)
+            },
+            'keyboard': {
+                'type': 'text'
+            }
         }
     )
